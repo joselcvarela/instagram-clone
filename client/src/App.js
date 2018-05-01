71b0883b3e194106d2ad89c9e6105a7c1cefea63
Jose Varela <joselcvarela@gmail.com>
2018-05-01 19:10:01 +0100

    Pass post attributes via props

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,12 @@ class App extends Component {
       <div>
         <Header />
         <div>
-          <Post />
+          <Post
+            nickname="José Varela"
+            avatar="https://media.licdn.com/dms/image/C4E03AQEWZFNLN-L9Vg/profile-displayphoto-shrink_200_200/0?e=1530381600&v=beta&t=V5NjqeB6r3rGJ-Kai3dH3nB9J3BzfIT_hC0mExJq_og"
+            image="https://cdn-images-1.medium.com/max/800/1*EOMP0V69RZ5xChG5pRoFyA.png"
+            caption="learning new tools"
+          />
         </div>
       </div>
     );
