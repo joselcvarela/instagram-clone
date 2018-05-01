03b59afd9fe92cbd6bc9569f500f8577ed60d6fe
Jose Varela <joselcvarela@gmail.com>
2018-05-01 19:06:25 +0100

    Add post component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
+import Post from './components/Post/Post'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <div>
+          <Post />
+        </div>
       </div>
     );
   }
